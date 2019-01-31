1514b0cb5be7447fe8f32d724c8519d1a6c0b8b9
Leela <leela.jagu@gmail.com>
2019-01-31 07:35:25 +0530

    Leg2

--- Mail Monkey/src/com/virtualpairprogrammers/webcontrollers/NewsletterController.java
+++ Mail Monkey/src/com/virtualpairprogrammers/webcontrollers/NewsletterController.java
@@ -1,7 +1,14 @@
 package com.virtualpairprogrammers.webcontrollers;
 
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,5 +18,27 @@ public class NewsletterController
 	public ModelAndView displayAllCustomersOnWebPage()
 	{
 		return new ModelAndView("/newsletter.jsp");
+	}
+	
+	@RequestMapping("/import.html")
+	public ModelAndView firstVersion(@RequestParam String code) {
+		RestTemplate template = new RestTemplate();
+		
+		String credentials = "mailmonkey:somesecretkey";
+		String encodedCredentials = Base64.encodeBase64String(credentials.getBytes());
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Basic " + encodedCredentials);
+		
+		HttpEntity entity = new HttpEntity<>(headers);
+		
+		String url ="http://localhost:8080/crm/oauth/token"
+				    + "?code=" + code
+				    +"&redirect_uri=http://localhost:8080/mailmonkey/import.html"  //not sure why we need to give redirect uri
+				    +"&grant_type=authorization_code";
+		
+		ResponseEntity<String> response = template.exchange(url, HttpMethod.POST, entity, String.class);
+		System.out.println(response.getBody());
+		return null;
 	}
 }
